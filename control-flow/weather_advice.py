a4ab455c25e290cabf3079042805c638994d57ff
Emmanuel047 <emwendwa007@gmail.com>
2025-09-17 09:26:26 +0300

    changed the prompt

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,4 @@
-weather = input("Whats the weather condition today: \n"
-                "1.Sunny\n"
-                "2.Rainy\n"
-                "3.Cold\n").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":

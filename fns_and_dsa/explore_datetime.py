bad4f677e336b727c51193e9182a4436d369c0b7
Emmanuel047 <emwendwa007@gmail.com>
2025-09-27 15:12:51 +0300

    refactor: rearrange functions and improve datetime display

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,15 @@
 import datetime
 from datetime import timedelta
-
-current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"Current date and time: {current_date}")
-
 def display_current_datetime():
-    Choice = int(input("Enter the number of days to add to the current date: "))
-    future_date = datetime.datetime.now() + timedelta(days=Choice)
-    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {current_date}")
+    
+display_current_datetime()
+
+def calculate_future_date():
+    choice = int(input("Enter the number of days to add to the current date: "))
+    future_date = datetime.datetime.now()+ timedelta(days=choice)
+    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
     return
 
-display_current_datetime()
+calculate_future_date()

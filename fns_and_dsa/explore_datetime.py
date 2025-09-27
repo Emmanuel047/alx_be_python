ad2b3e5e9094e7c11982ec9db434c7b4a189e93d
Emmanuel047 <emwendwa007@gmail.com>
2025-09-27 12:57:51 +0300

    date function rename

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,10 +4,10 @@ from datetime import timedelta
 current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"Current date and time: {current_date}")
 
-def calculate_future_date():
+def display_current_datetime():
     Choice = int(input("Enter the number of days to add to the current date: "))
     future_date = datetime.datetime.now() + timedelta(days=Choice)
     print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
     return
 
-calculate_future_date()
+display_current_datetime()

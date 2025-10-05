108e7d08882ceb71c18bc943aa1e73c0df541df9
Emmanuel047 <emwenwa007@gmail.com>
2025-10-05 22:46:48 +0300

    bank2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        return f"Current balance: ${self.account_balance}"
+        return f"Current balance: {self.account_balance}"

931ff5d59ca84d288a45c362ac4544d88d06f43c
Emmanuel047 <emwenwa007@gmail.com>
2025-10-05 22:38:33 +0300

    Bank

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance, initial_balance = 0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        amount += self.account_balance
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        return f"Current balance: ${self.account_balance}"
